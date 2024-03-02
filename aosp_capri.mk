6bbdbf28fb2dc1ed4bb88040b3de540f1bd8fb66
TogoFire <togofire@mailfence.com>
2024-03-02 10:51:56 -0500

    capri: introduce Viper4Android FX
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: sabrina010209 <mcpe11640@gmail.com>

--- aosp_capri.mk
+++ aosp_capri.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/motorola/capri/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
+# Inherit from ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_capri
 PRODUCT_DEVICE := capri
