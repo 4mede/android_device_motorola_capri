d6b63afb77027e6337a8caf8f82f6d2964102c83
Deivid21 <david.parra.ignacio@gmail.com>
2025-02-20 01:51:50 -0300

    capri: Build MotCamera3 from prebuilt source
    Change-Id: Id16eaa3f0732361b3d5f594985d9bffde18a2236
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.bengal
 
+# Moto Camera 3
+TARGET_MOTCAMERA3 := capri
+TARGET_USES_MOTCAMERA3 := true
+
+$(call inherit-product, vendor/motorola/MotCamera3/motcamera3.mk)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service
